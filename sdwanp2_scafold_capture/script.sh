2ca8d4e63902e8b4414e08499531c1020ac27371
Denny Rizky Ramdani <denny@DESKTOP-LMN9F9I.localdomain>
2022-06-30 21:06:56 +0700

    Functioning script done for sdwan gadai capture scafold

--- sdwanp2_scafold_capture/script.sh
+++ sdwanp2_scafold_capture/script.sh
@@ -1,58 +1,25 @@
 #!/bin/bash
-makefolder () {
-    input="$1"
-    while IFS= read -r line
+
+scafoldcapture () {
+    workingdir=$(pwd)
+    while IFS="," read -r -a line
     do
-        echo "$line"
+        cd "$workingdir"
+        max=$((${#line[@]}-1))
+        for i in $(seq 0 $max)
+        do
+            if (($i == 0))
+            then
+                mkdir "${line[i]}"
+                cd "${line[i]}"
+                echo "${line[i]}"
+            else
+                touch "${line[i]}"
+                echo "${line[i]}"
+            fi
+        done
         echo
-        # ls "/home/denny/windoc/!PSI!/2022/Pegadaian/SDWAN P2/migrasi/CP Jambi/$line"
-    done < "$input"
-    ls
-    pwd
+    done < "main_file"
 }
 
-makefolder "$1"
-
-
-# mkdir '1 Capture Before Only LTE'
-# mkdir '2 Capture After with WAN migrated'
-# mkdir '3 Capture Test LTE off WAN on'
-# mkdir '4 Capture Test LTE on WAN off'
-# mkdir '5 Capture Final LTE on WAN on'
-
-# cd '1 Capture Before Only LTE'
-# touch 'show ip interface brief.png'
-# touch 'show ip route vrf 10 summary.png'
-# touch 'show ip route.png'
-# touch 'show sdwan bfd sessions.png'
-# touch 'show sdwan control connections.png'
-# touch 'show sdwan omp peers.png'
-# cd '../2 Capture After with WAN migrated'
-# touch 'show ip interface brief.png'
-# touch 'show ip route vrf 10 summary.png'
-# touch 'show ip route.png'
-# touch 'show sdwan bfd sessions.png'
-# touch 'show sdwan control connections.png'
-# touch 'show sdwan omp peers.png'
-# cd '../3 Capture Test LTE off WAN on'
-# touch 'show ip interface brief.png'
-# touch 'show ip route vrf 10 summary.png'
-# touch 'show ip route.png'
-# touch 'show sdwan bfd sessions.png'
-# touch 'show sdwan control connections.png'
-# touch 'show sdwan omp peers.png'
-# cd '../4 Capture Test LTE on WAN off'
-# touch 'show ip interface brief.png'
-# touch 'show ip route vrf 10 summary.png'
-# touch 'show ip route.png'
-# touch 'show sdwan bfd sessions.png'
-# touch 'show sdwan control connections.png'
-# touch 'show sdwan omp peers.png'
-# cd '../5 Capture Final LTE on WAN on'
-# touch 'show ip interface brief.png'
-# touch 'show ip route vrf 10 summary.png'
-# touch 'show ip route.png'
-# touch 'show sdwan bfd sessions.png'
-# touch 'show sdwan control connections.png'
-# touch 'show sdwan omp peers.png'
-# touch 'show cdp neigbors.png'
+scafoldcapture
