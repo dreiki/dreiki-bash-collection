660b70949aa37ce6714138561ef8902e583d3741
Denny Rizky Ramdani <denny@DESKTOP-LMN9F9I.localdomain>
2022-07-04 10:03:07 +0700

    sdwan capture scafold: add optional path parameter

--- sdwanp2_scafold_capture/script.sh
+++ sdwanp2_scafold_capture/script.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 scafoldcapture () {
-    workingdir=$(pwd)
+    workingdir="${1:-$(pwd)}"
+    echo "$workingdir"
+    # ls "$workingdir"
+    # workingdir=$(pwd)
+    # echo $workingdir
     while IFS="," read -r -a line
     do
         cd "$workingdir"
         max=$((${#line[@]}-1))
         for i in $(seq 0 $max)
         do
-            if (($i == 0))
-            then
+            if (($i == 0)) # check field
+            then # make parent directory
                 mkdir "${line[i]}"
                 cd "${line[i]}"
                 echo "${line[i]}"
-            else
+            else # touch the file required
                 touch "${line[i]}"
                 echo "${line[i]}"
             fi
@@ -21,5 +25,5 @@ scafoldcapture () {
         echo
     done < "main_file"
 }
-
-scafoldcapture
+scafoldcapture "$1"
+# scafoldcapture
